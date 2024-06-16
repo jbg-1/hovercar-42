e0a55d11be6581be41899d9db2c504f74a0ca7d3
Jonas Kampshoff <jonas.kampshoff@study.hs-duesseldorf.de>
2024-06-17 00:01:12 +0200

    carControllerChanges

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,6 +1,18 @@
 using Unity.Netcode;
 using UnityEngine;
 
+[System.Serializable]
+public struct CarSettings
+{
+    public float forewardTurbineStrength;
+    public float upwardTurbineStrength;
+    public float maxSpeed; //The maximum Speed the foreward acceleration is working
+    public float rotationSpeedCarVelocityInfluence;
+    public float airStabilisationRotationStrength;
+    public float groundStabilisationRotationStrength;
+}
+
+
 public class CarController : NetworkBehaviour
 {
     public static int LastCheckpointCollected = 0;
@@ -9,20 +21,9 @@ public class CarController : NetworkBehaviour
     private Vector3 lastCheckpointRotation;
     [SerializeField] bool debugMode = true;
 
+    [SerializeField] private CarSettings carSettings;
 
-    [Header("CarSettings")]
-    [Header("Speed")]
-    [SerializeField] private float maxSpeed = 100f;
-    [SerializeField] private float horizontalForwardAcceleration = 10;
-    [SerializeField] private float horizontalDriftDamping = 10;
-    [Tooltip("The accerleration")]
-    [SerializeField] private float verticalRotationSpeed = 30;
-    [SerializeField] private float rotationSpeedCarVelocityInfluence = 0.1f;
-    [SerializeField] private float rotationSpeed = 15;
-
-    [Header("Flying")]
     [SerializeField] private Vector3 gravity;
-    [SerializeField] private float upwardsAcceleration = 20;
     [SerializeField] private float flyingHeight = 2f;
     [SerializeField] private LayerMask groundMask;
 
@@ -38,8 +39,7 @@ public class CarController : NetworkBehaviour
     [SerializeField] private GameObject LeftBackTurbine;
 
 
-    private Vector3 accelerationMovement = Vector3.zero;
-    private float accelerationRotation = 0;
+    private float rotationInput;
 
     private void Start()
     { 
@@ -56,20 +56,11 @@ public class CarController : NetworkBehaviour
         {
             if (debugMode)
             {
-                accelerationRotation += -Input.GetAxis("Horizontal") * verticalRotationSpeed * (1 + carRigidbody.velocity.magnitude * rotationSpeedCarVelocityInfluence) * Time.deltaTime;
+                rotationInput = Input.GetAxis("Horizontal");
             }
             else
             {
-                accelerationRotation += Angle(AppInputController.Orientation) * verticalRotationSpeed * (1 + carRigidbody.velocity.magnitude * rotationSpeedCarVelocityInfluence) * Time.deltaTime;
-            }
-            Vector3 parallelVector = (Vector3.Dot(transform.forward, carRigidbody.velocity)) * transform.forward;
-            Vector3 orthogonalVector = carRigidbody.velocity - parallelVector;
-            
-            accelerationMovement -=  horizontalDriftDamping * Time.deltaTime * orthogonalVector;
-            //forewardAcceleration
-            if (parallelVector.sqrMagnitude < maxSpeed * maxSpeed)
-            {
-                accelerationMovement += horizontalForwardAcceleration * Time.deltaTime * transform.forward;
+                rotationInput = Angle(AppInputController.Orientation);
             }
         }
     }
@@ -78,67 +69,66 @@ public class CarController : NetworkBehaviour
     {
         if (IsOwner)
         {
-            float leftFrontHeight = flyingHeight * 2;
-            float rightFrontHeight = flyingHeight * 2;
-            float leftBackHeight = flyingHeight * 2;
-            float rightBackHeight = flyingHeight * 2;
-            if (Physics.Raycast(LeftFrontTurbine.transform.position, gravity, out RaycastHit hitLF, flyingHeight * 2, groundMask))
+            Vector3 parallelVector = (Vector3.Dot(transform.forward, carRigidbody.velocity)) * transform.forward;
+            Vector3 sideDriftVector = (Vector3.Dot(transform.right, carRigidbody.velocity)) * transform.right;
+
+            float forewardvelocity = parallelVector.magnitude;
+
+            Vector3 acceleration = -sideDriftVector;
+
+            if(forewardvelocity < carSettings.maxSpeed)
+            {
+                acceleration += carSettings.forewardTurbineStrength * transform.forward;
+            }
+
+            //Steereing
+            float rotationAcceleration = rotationInput * 1;
+
+
+            float leftFrontHeight = flyingHeight * 4;
+            float rightFrontHeight = flyingHeight * 4;
+            float leftBackHeight = flyingHeight * 4;
+            float rightBackHeight = flyingHeight * 4;
+            if (Physics.Raycast(LeftFrontTurbine.transform.position, -transform.up, out RaycastHit hitLF, flyingHeight * 2, groundMask))
             {
                 leftFrontHeight = hitLF.distance;
             }
-            if (Physics.Raycast(RightFrontTurbine.transform.position, gravity, out RaycastHit hitRF, flyingHeight * 2, groundMask))
+            if (Physics.Raycast(RightFrontTurbine.transform.position, -transform.up, out RaycastHit hitRF, flyingHeight * 2, groundMask))
             {
                 rightFrontHeight = hitRF.distance;
-            }
-            if (Physics.Raycast(LeftBackTurbine.transform.position, gravity, out RaycastHit hitLB, flyingHeight * 2, groundMask))
+            } 
+            if (Physics.Raycast(LeftBackTurbine.transform.position, -transform.up, out RaycastHit hitLB, flyingHeight * 2, groundMask))
             {
                 leftBackHeight = hitLB.distance;
             }
-            if (Physics.Raycast(RightBackTurbine.transform.position, gravity, out RaycastHit hitRB, flyingHeight * 2, groundMask))
+            if (Physics.Raycast(RightBackTurbine.transform.position, -transform.up, out RaycastHit hitRB, flyingHeight * 2, groundMask))
             {
                 rightBackHeight = hitRB.distance;
-            }
+            }     
             float total = (leftFrontHeight + rightFrontHeight + leftBackHeight + rightBackHeight) / 4;
-            Vector3 gravityToAdd; 
-            if (total < flyingHeight)
+
+            if(total < flyingHeight)
             {
-                gravityToAdd = -upwardsAcceleration * (1 - total / flyingHeight) * gravity;
+                acceleration += (1 - total / flyingHeight) * carSettings.upwardTurbineStrength * transform.up;
             }
-            else
-            {
-                gravityToAdd = gravity;
-            }
+            acceleration += gravity;
+
+
             float turnRight;
             float turnForward;
-            if (total < flyingHeight * 1.1)
+            if (total < flyingHeight * 1.5)
             {
-                if ((leftFrontHeight + leftBackHeight) / 2 > (rightBackHeight + rightFrontHeight) / 2)
-                {
-                    turnRight = Time.fixedDeltaTime * rotationSpeed;
-                }
-                else
-                {
-                    turnRight = -Time.fixedDeltaTime * rotationSpeed;
-                }
-
-                if ((leftFrontHeight + rightFrontHeight) / 2 > (leftBackHeight + rightBackHeight) / 2)
-                {
-                    turnForward = Time.fixedDeltaTime * rotationSpeed;
-                }
-                else
-                {
-                    turnForward = -Time.fixedDeltaTime * rotationSpeed;
-                }
+                turnRight = carSettings.groundStabilisationRotationStrength * ((leftFrontHeight + leftBackHeight) - (rightBackHeight + rightFrontHeight));
+                turnForward = carSettings.groundStabilisationRotationStrength * ((leftFrontHeight + rightFrontHeight) - (leftBackHeight + rightBackHeight));
             }
             else
             {
-                turnForward = -Vector3.Dot(gravity, transform.forward) * Time.fixedDeltaTime * rotationSpeed;
-                turnRight = Vector3.Dot(gravity, transform.right) * Time.fixedDeltaTime * rotationSpeed;
+                turnForward = -Vector3.Dot(gravity, transform.forward) * carSettings.airStabilisationRotationStrength;
+                turnRight = Vector3.Dot(gravity, transform.right)  * carSettings.airStabilisationRotationStrength;
             }
-            carRigidbody.AddRelativeTorque(new Vector3(turnForward, accelerationRotation, turnRight), ForceMode.Acceleration);
-            accelerationRotation = 0;
-            carRigidbody.AddForce(accelerationMovement + gravityToAdd * Time.fixedDeltaTime, ForceMode.Acceleration);
-            accelerationMovement = Vector3.zero;
+      
+            carRigidbody.AddRelativeTorque(new Vector3(turnForward, rotationAcceleration, turnRight), ForceMode.Acceleration);
+            carRigidbody.AddForce(acceleration, ForceMode.Acceleration);
         }    
     }
 
