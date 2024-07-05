8ebb905c126eef0e1705712598eb29f93e995258
noah.hrbth <noah.harborth.private@gmail.com>
2024-07-06 01:24:43 +0200

    add function to detect if player drives in wrong direction

--- Assets/Scripts/CheckpointLogic.cs
+++ Assets/Scripts/CheckpointLogic.cs
@@ -5,56 +5,74 @@ using static FinishTimer;
 [System.Serializable]
 public class CheckpointLogic : MonoBehaviour
 {
-    [SerializeField] GameObject checkPointParent;
+  [SerializeField] GameObject checkPointParent;
 
-    public Checkpoint[] checkpoints;
+  public Checkpoint[] checkpoints;
 
-    public Dictionary<int,int> checkPointCount = new Dictionary<int, int>();
+  public Dictionary<int, int> checkPointCount = new Dictionary<int, int>();
 
-    public event OnFinished onFinish;
+  public delegate void OnFinished(int carId);
+  public event OnFinished onFinish;
+  public delegate void OnWrongDirection(int carId);
+  public event OnWrongDirection onWrongDirection;
 
 
-    private void Start()
+  private void Start()
+  {
+    checkpoints = checkPointParent.GetComponentsInChildren<Checkpoint>();
+
+    for (int i = 0; i < checkpoints.Length; i++)
     {
-        checkpoints = checkPointParent.GetComponentsInChildren<Checkpoint>();
+      checkpoints[i].SetID(i);
+      checkpoints[i].SetCheckpointLogic(this);
+    }
+  }
 
-        for (int i = 0; i < checkpoints.Length; i++) { 
-            checkpoints[i].SetID(i);
-            checkpoints[i].SetCheckpointLogic(this);
-        }
+  public void NotifyTrigger(int checkPointId, CarController carController)
+  {
+    if (IsDrivinWrongDirection(checkPointId, carController))
+    {
+      onWrongDirection?.Invoke(carController.carId);
     }
 
-    public void NotifyTrigger(int checkPointId, CarController carController)
+    if (!checkPointCount.ContainsKey(carController.carId))
     {
-        if (!checkPointCount.ContainsKey(carController.carId))
-        {
-            checkPointCount.Add(carController.carId, 0);
-            carController.SetLastCheckpoint(checkpoints[checkPointId].transform.position, checkpoints[checkPointId].transform.rotation.eulerAngles);
-        }
-        else
-        {
-            if (checkPointId == (checkPointCount[carController.carId] + 1) % checkpoints.Length)
-            {
-                checkPointCount[carController.carId] = checkPointCount[carController.carId] + 1;
+      checkPointCount.Add(carController.carId, 0);
+      carController.SetLastCheckpoint(checkpoints[checkPointId].transform.position, checkpoints[checkPointId].transform.rotation.eulerAngles);
+    }
+    else
+    {
+      if (checkPointId == (checkPointCount[carController.carId] + 1) % checkpoints.Length)
+      {
+        checkPointCount[carController.carId] = checkPointCount[carController.carId] + 1;
 
-                carController.SetLastCheckpoint(checkpoints[checkPointId].transform.position + new Vector3(0, 5, 0), checkpoints[checkPointId].transform.rotation.eulerAngles);
-                Debug.Log("Checkpoint " + checkPointId + " collected; count " + checkPointCount[carController.carId]);
-            }
+        carController.SetLastCheckpoint(checkpoints[checkPointId].transform.position + new Vector3(0, 5, 0), checkpoints[checkPointId].transform.rotation.eulerAngles);
+        Debug.Log("Checkpoint " + checkPointId + " collected; count " + checkPointCount[carController.carId]);
+      }
 
-            if (checkPointCount[carController.carId]/checkpoints.Length == 1)
-            {
-                onFinish(carController.carId);
-            }
+      if (checkPointCount[carController.carId] / checkpoints.Length == 1)
+      {
+        onFinish(carController.carId);
+      }
+    }
+  }
 
-        }
-        
+  public GameObject getCheckpoint(int checkpointCount)
+  {
+    return checkpoints[checkpointCount % checkpoints.Length].gameObject;
+  }
+
+
+  private bool IsDrivinWrongDirection(int checkPointId, CarController carController)
+  {
+    if (checkPointCount.ContainsKey(carController.carId))
+    {
+      if (checkPointId != (checkPointCount[carController.carId] + 1) % checkpoints.Length)
+      {
+        return true;
+      }
     }
 
-    public GameObject getCheckpoint(int checkpointCount)
-    {
-        return checkpoints[checkpointCount % checkpoints.Length].gameObject;
-    }
-
-    public delegate void OnFinished(int carId);
-
+    return false;
+  }
 }
