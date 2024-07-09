a524edbe6643aa18587bed7208162306a7372eb7
noah.hrbth <noah.harborth.private@gmail.com>
2024-07-09 21:44:17 +0200

    add item display

--- Assets/Scripts/CheckpointLogic.cs
+++ Assets/Scripts/CheckpointLogic.cs
@@ -19,7 +19,7 @@ public class CheckpointLogic : MonoBehaviour
   //only for wrong direction detection
   private List<int> collectedCheckpoints = new List<int>();
 
-    private void Start()
+  private void Start()
   {
     checkpoints = checkPointParent.GetComponentsInChildren<Checkpoint>();
     totalCheckpointAmount = checkpoints.Length;
@@ -38,6 +38,27 @@ public class CheckpointLogic : MonoBehaviour
     collectedCheckpoints.Add(checkPointId);
 
     hud.ToggleWrongDirectionText(IsDrivinWrongDirection(checkPointId, carController));
+
+    if (checkPointId == 2)
+    {
+      hud.ToggleItemDisplay(true, "banana");
+    }
+    else if (checkPointId == 4)
+    {
+      hud.ToggleItemDisplay(true, "bomb");
+    }
+    else if (checkPointId == 6)
+    {
+      hud.ToggleItemDisplay(true, "ice-cube");
+    }
+    else if (checkPointId == 7)
+    {
+      hud.ToggleItemDisplay(true, "thunder");
+    }
+    else
+    {
+      hud.ToggleItemDisplay(false);
+    }
 
     if (collectedCheckpoints.Count == totalCheckpointAmount - 1)
     {
@@ -62,10 +83,12 @@ public class CheckpointLogic : MonoBehaviour
       if (checkPointCount[carController.carId] == totalCheckpointAmount)
       {
         hud.UpdateRounds(2);
-      } else if (checkPointCount[carController.carId] == totalCheckpointAmount * 2)
+      }
+      else if (checkPointCount[carController.carId] == totalCheckpointAmount * 2)
       {
         hud.UpdateRounds(3);
-      } else if (checkPointCount[carController.carId] == totalCheckpointAmount * 3)
+      }
+      else if (checkPointCount[carController.carId] == totalCheckpointAmount * 3)
       {
         onFinish(carController.carId);
       }
@@ -76,7 +99,7 @@ public class CheckpointLogic : MonoBehaviour
   {
     return checkpoints[checkpointCount % checkpoints.Length].gameObject;
   }
-int count = 0;
+  int count = 0;
   private bool IsDrivinWrongDirection(int checkPointId, CarController carController)
   {
     count++;
