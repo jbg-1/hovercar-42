ed3540eb25565eb786d609bc70663451d9e9386b
TillionR <till.rogoll@study.hs-duesseldorf.de>
2024-07-11 10:56:41 +0200

    add two more items, one without logic

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -280,4 +280,20 @@ public class CarController : NetworkBehaviour
         carRigidbody.isKinematic = false;
     }
 
+    public void Lightning() 
+    {
+        carRigidbody.velocity = Vector3.zero;
+        carRigidbody.isKinematic = true;
+    }
+
+    public void switchPositionWihtOtherCar(CarController otherCar)
+    {
+        //Keine Ahnung ob das Funktioniert das war Copilot
+        Vector3 tempPosition = otherCar.transform.position;
+        otherCar.transform.position = this.transform.position;
+        this.transform.position = tempPosition;
+    }
+
+
+
 }
---
+++ Assets/Scripts/Items/SwitchCarItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCarItem : Item
+{
+    public override void useItem(ItemController itemController)
+    {
+        Debug.Log("SwitchCarItem used");
+        itemController.gameObject.GetComponent<CarController>().switchPositionWihtOtherCar();
+    }
+}
