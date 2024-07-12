520851a07b1f539f8f5e3a32997c10417d04ce35
TillionR <till.rogoll@study.hs-duesseldorf.de>
2024-07-12 10:23:24 +0200

    Update ItemController.cs

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -5,6 +5,7 @@ public class ItemController : MonoBehaviour
 {
     public Item item;
     private Coroutine autoUseItemCoroutine;
+    private Coroutine displayRandomItemsCoroutine;
 
     // Method to use the item
     public void UseItem()
@@ -29,48 +30,83 @@ public class ItemController : MonoBehaviour
 
     public void collectItem()
     {
-        Debug.Log("Item collected");
-        CarController carController = GetComponent<CarController>();
-
         if (item == null)
         {
             int random = Random.Range(1, 5);
             switch (random)
             {
                 case 1:
-                    HUD.instance.ToggleItemDisplay(true, "banana");
                     item = new BoostItem();
                     break;
                 case 2:
-                    HUD.instance.ToggleItemDisplay(true, "ice-cube");
                     item = new FreezeItem();
                     break;
                 case 3:
-                    HUD.instance.ToggleItemDisplay(true, "thunder");
                     item = new LightningItem();
                     break;
                 case 4:
-                    HUD.instance.ToggleItemDisplay(true, "bomb");
                     item = new SwitchCarItem();
                     break;
             }
 
+            // Start the coroutine to display random items for 2 seconds
+            if (displayRandomItemsCoroutine != null)
+            {
+                StopCoroutine(displayRandomItemsCoroutine);
+            }
+            displayRandomItemsCoroutine = StartCoroutine(DisplayRandomItemsForDuration(2f));
+        }
+    }
+
+    private IEnumerator DisplayRandomItemsForDuration(float duration)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += 0.2f; // Change item every 0.2 seconds
+            int random = Random.Range(1, 5);
+            switch (random)
+            {
+                case 1:
+                    HUD.instance.ToggleItemDisplay(true, "banana");
+                    break;
+                case 2:
+                    HUD.instance.ToggleItemDisplay(true, "ice-cube");
+                    break;
+                case 3:
+                    HUD.instance.ToggleItemDisplay(true, "thunder");
+                    break;
+                case 4:
+                    HUD.instance.ToggleItemDisplay(true, "bomb");
+                    break;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        // Display the actual item
+        if (item != null)
+        {
+            if (item is BoostItem)
+                HUD.instance.ToggleItemDisplay(true, "banana");
+            else if (item is FreezeItem)
+                HUD.instance.ToggleItemDisplay(true, "ice-cube");
+            else if (item is LightningItem)
+                HUD.instance.ToggleItemDisplay(true, "thunder");
+            else if (item is SwitchCarItem)
+                HUD.instance.ToggleItemDisplay(true, "bomb");
+
             // Start the coroutine to use the item after 3 seconds
             if (autoUseItemCoroutine != null)
             {
-                Debug.Log("Item Stop auto use item coroutine");
                 StopCoroutine(autoUseItemCoroutine);
             }
-            Debug.Log("Item Start auto use item coroutine");
             autoUseItemCoroutine = StartCoroutine(AutoUseItemAfterDelay(3f));
         }
     }
 
     private IEnumerator AutoUseItemAfterDelay(float delay)
     {
-        Debug.Log("Item Auto use item after delay");
         yield return new WaitForSeconds(delay);
         UseItem();
     }
 }
-
